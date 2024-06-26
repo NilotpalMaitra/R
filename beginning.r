7b3fe65410ce2f28cafbd09faa24af356cb9fc0e
Nilvalox <nilotpalm2015@gmail.com>
2024-06-26 20:18:26 +0530

    commit

---
+++ beginning.r
@@ -0,0 +1,9 @@
+name <- c("Greg", "Marcia", "Peter", "Jan", "Bobby", "Cindy")
+
+age <- c(14, 12, 11, 10, 8, 6)
+gender <- c("M", "F", "M", "F", "M", "F")
+
+friends <- data.frame(name, age, gender)
+
+
+friends$age
