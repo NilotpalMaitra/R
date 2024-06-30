c50152502c96f251a3ed224f12874c02917927bc
Nilvalox <nilotpalm2015@gmail.com>
2024-06-30 17:18:26 +0530

    commit

--- chisquaredtest.r
+++ chisquaredtest.r
@@ -1,7 +1,11 @@
-#chi-squared goodnest of fit test
+# Create a contingency table
+data <- matrix(c(10, 20, 30, 40, 50, 60), nrow = 2, byrow = TRUE)
+colnames(data) <- c("Category1", "Category2", "Category3")
+rownames(data) <- c("Group1", "Group2")
+data
 
-library(tidyverse)
+# Perform the Chi-Squared test
+chisq_test <- chisq.test(data)
 
-
-
-
+# Display the test result
+print(chisq_test)
