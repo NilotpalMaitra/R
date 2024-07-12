0ac667f5c59a783a92c24e87f4603deed72d0e3e
Nilvalox <nilotpalm2015@gmail.com>
2024-07-12 13:23:36 +0530

    commit

---
+++ commenting.r
@@ -0,0 +1,12 @@
+#Commenting in R language 
+
+print ("Commenting in R language")
+
+# Check if the condition is TRUE
+if (TRUE) {
+  # This is a comment
+  print("This is a comment")
+} else {
+  # This is not a comment
+  print("This is not a comment")
+}
